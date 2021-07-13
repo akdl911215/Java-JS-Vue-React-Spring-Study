83c76fbb0d7867b8435d77b5ea5824ffcc69f9b5
LEE Jung Hyun <akdl911215@gmail.com>
2021-07-13 19:04:11 +0900

    !

---
+++ JAVA2/src/compare.java
@@ -0,0 +1,21 @@
+public class compare {
+    public static void main(String[] args) {
+        String str = "abcd";
+        System.out.println("str : " + str);
+        System.out.println("str address : " + System.identityHashCode(str));
+        System.out.println("");
+
+        String str2 = new String("abcd");
+        System.out.println("str2 : " + str2);
+        System.out.println("str2 address : " + System.identityHashCode(str2));
+
+        if (str == str2) {
+            System.out.println("");
+            System.out.println("두개의 값이 같다");
+        }
+        else {
+            System.out.println("");
+            System.out.println("두개의 값이 다르다.");
+        }
+    }
+}
---
+++ JAVA2/src/compare3.java
@@ -0,0 +1,14 @@
+public class compare3 {
+    public static void main(String[] args) {
+        String str = "Test String";
+        System.out.println("str : " + str);
+        System.out.println("str address : " + System.identityHashCode(str));
+
+        String str2 = new String("Test String");
+        System.out.println("\nstr2 : " + str2);
+        System.out.println("str2 address : " + System.identityHashCode(str2));
+
+        System.out.println(" ");
+        System.out.println(str.equals(str2));
+    }
+}
