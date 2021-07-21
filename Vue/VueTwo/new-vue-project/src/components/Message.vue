d0b5346db564cb8193b1a21732c734bb87e5a5ff
LEE Jung Hyun <akdl911215@gmail.com>
2021-07-21 23:40:22 +0900

    vue setting

---
+++ Vue/VueTwo/new-vue-project/src/components/Message.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app">
+        {{ message }}
+    </div>
+</template>
+
+<script>
+export default{
+    name: '#app',
+    data: {
+        message: '반갑고 VUE'
+    }
+}
+</script>
