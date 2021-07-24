1266334b41f245bbdd9567b0fcdf485f4865bf03
LEE Jung Hyun <akdl911215@gmail.com>
2021-07-24 16:21:47 +0900

    1

--- JAVASCRIPT/JavaScriptDeepDive/deepDive/chapter-9/ex09-12.js
+++ JAVASCRIPT/JavaScriptDeepDive/deepDive/chapter-9/ex09-12.js
@@ -1,9 +1,14 @@
 // 단축평가 사용법 1.
 // 객체를 가리키기를 기대하는 변수가 null 또는 undefined가 아닌지 확인하고 프로퍼티를 참조할 때
 
-var elem = null;
-// elem이 null 이나 undefined와 같은 Falsy 값이면 elem으로 평가되고
-// elem이 Truthy 값이면 elem.value로  평가된다.
+// var elisiroom = null;
 
-var value = elem && elem.value;
-console.log(typeof value, value) // object null
+// var value = elisiroom.value;
+// console.log(typeof value, value);
+
+var elisiroom = null;
+// elisiroom이 null 이나 undefined와 같은 Falsy 값이면 elisiroom으로 평가되고
+// elisiroom이 Truthy 값이면 elisiroom.value로  평가된다.
+
+var value = elisiroom && elisiroom.value;
+console.log('value값 : ', typeof value, value); // object null
---
+++ Vue/VueTwo/new-vue-project/src/todo/TodoInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h3>Todo 입력창</h3>
+        <input type="text" v-model="newTodoItem" v-on:keypress.enter="addTodo">
+        <button v-on:click="addTodo">추가</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        retrun {
+            newTodoItem: ''
+        }
+    },
+    methods: {
+        addTodo () {
+            if (this.newTodoItem !== '') {
+                var value = this.newTodoItem && this.newTodoItem.trim()
+                console.log('addTodo')
+                this.$emit('addTodo', value)
+                this.clearInput()
+            }
+        },
+        clearImput () {
+            this.newTodoItem = ''
+        }
+    }
+
+}
+</script>
