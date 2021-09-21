d1c6c24086646708ef649eb7af9ddea1afa4008b
akdl911215 <akdl911215@gmail.com>
2021-09-21 22:31:26 +0900

    jsp

--- JSP/HelloJSP/JSPcoding/src/main/java/com/servlet/test/HelloServlet.java
+++ JSP/HelloJSP/JSPcoding/src/main/java/com/servlet/test/HelloServlet.java
@@ -16,12 +16,31 @@ public class HelloServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		java.io.PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1>doGet() 메서드</h1>");
+		String name = request.getParameter("name");
+		out.println(name);
+		out.println("</body></html>");
+		out.close();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //		doGet(request, response);
-		response.getWriter().append("(POST)Served at: ").append(request.getContextPath());
+//		response.getWriter().append("(POST)Served at: ").append(request.getContextPath());
+		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		java.io.PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1>doPost() 메서드");
+		String name = request.getParameter("name");
+		out.println(name);
+		out.println("</body></html>");
+		out.close();
 	}
 
 }
