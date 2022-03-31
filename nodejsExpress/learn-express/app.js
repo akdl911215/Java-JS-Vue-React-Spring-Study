8adf867c02fee703326d779f4dc1e36c31317c0d
akdl911215 <akdl911215@gmail.com>
2022-03-31 14:25:45 +0900

    node

--- nodejsExpress/learn-express/app.js
+++ nodejsExpress/learn-express/app.js
@@ -3,6 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
+// flash : 일회성 메시지들을 웹 브라우저에 나타낼 때 사용
+var flash = require("connect-flash");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -20,20 +23,57 @@ app.set("view engine", "pug");
 
 app.use(function (req, res, next) {
   console.log(req.url, "저도 미들웨어입니다.");
-  //next();
+  next();
+});
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "sceret code",
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+app.use(flash());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// 404 처리 미들웨어
+app.use(function (req, rs, next) {
+  next(createError(404));
+});
+
+// 에러 핸들러
+app.use(function (err, req, res, next) {
+  // set locals, only prioviding error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 // 2 end
 
 // 3 start
 // 중간 부분에 app.use로 시작하는 코드가 많은데, 미들웨어를 연결하는 부분이다.
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger("dev")); // dev 대신 short, common, combined 등을 줄 수 있다.
+
+// static 미들웨어는 정적인 파일들을 제공
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// express 4.16.0 버전부터는 body-arser의 일부 기능이 익스프레스에 내장되었기에 설치하지 않고
+// express.json() 과 express.urlencoded({ extended: false }) 사용 가능
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// 요청에 등봉된 쿠키를 해석
+app.use(cookieParser("secret code"));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -50,6 +90,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
 // 3 end
 
 // 4 start
--- nodejsExpress/learn-express/routes/users.js
+++ nodejsExpress/learn-express/routes/users.js
@@ -6,4 +6,20 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+router.get("/flash", function (req, res) {
+  req.session.message = "세션 메시지";
+  req.flash("message", "flash 메시지");
+  res.redirect("/users/flash/result");
+});
+
+router.get("/flash/result", function (req, res) {
+  console.log("req", req.session);
+
+  res.send(`${req.session.message} ${req.flash("message")}`);
+});
+
+router.get("/users/:id", function (req, res) {
+  fconsole.log("req.params : ", req.params, ", req.query : ", req.query);
+});
+
 module.exports = router;
