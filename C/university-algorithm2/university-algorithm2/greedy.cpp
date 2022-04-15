df20e05171c43abc391bc0a359f9c8fe60b66554
akdl911215 <akdl911215@naver.com>
2022-04-15 23:51:33 +0900

    teting algorithm

---
+++ C/university-algorithm2/university-algorithm2/greedy.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int M = 10; // 배낭의 무게
+	int n = 4; // 물건의 갯수
+	int p[4] = { 14, 15, 20, 9 }; // 물체 i의 이익
+	int w[4] = { 4, 3, 5, 3 }; // 물체 i의 무게
+	double result[4];
+	int rangking[4];
+	int x[4];// 배낭에 들어간 물체 i의 비율
+	
+
+	for (int i = 0; i < n; ++i) {
+		result[i] = (double)p[i] / (double)w[i];
+		cout << result[i] << endl;
+		rangking[i] = 0;
+		x[i] = 0;
+	}
+
+	for (int i = 0; i < n; ++i) {
+		int rank = 1;
+		for (int j = 0; j < n; ++j) {
+			if (result[i] > result[j]) {
+				rangking[i] = rank++;
+			}
+		} 
+	}
+
+	for (int i = 0; i < n; ++i)
+		cout << rangking[i] << " ";
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			if (i == rangking[j]) {
+				int r = M;
+				for (int i = 0; i < n; ++i) {
+					if (w[i] < r) {
+
+					}
+				}
+			}
+		}
+	}
+
+	return 0;
+}
